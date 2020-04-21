e989adfe70f13ffd29e65b77fbd3cb824fe00567
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-21 21:39:18 +0200

    Async and await

--- index.js
+++ index.js
@@ -37,12 +37,13 @@ function getRepositories(username, callback) {
 // 2. Promises
 
 // Consume promises
-getUser(1)
+
+/* getUser(1)
     .then(user => getRepositories(user.gitHubUsername))
     .then(repos => getCommits(repos[0]))
     .then(commits => console.log('Commits', commits))
     .catch(err => console.log('Error', err.message));
-
+*/
 function getUser(id) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -72,5 +73,18 @@ function getCommits(repo) {
 }
 
 // 3. Async/await
+
+// Async and Await approach
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    } catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
 
 console.log('After');
