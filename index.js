a4d07e146d8e40f0805215f501edc21bae5b5b56
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-21 17:05:59 +0200

    Sync vs async code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Before');
+console.log("before");
+//Async code: will be executed later
+setTimeout(() => {
+    console.log('Reading a user from a database...');
+}, 2000);
+
+console.log("after");
