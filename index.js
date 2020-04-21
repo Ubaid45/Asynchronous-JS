6cda3875a4a8e25f9f1cd1ba87382294713b57b7
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-21 17:12:23 +0200

    Patterns for dealing with async code. 1. Callbacks 2. Promises 3. Async/await

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 console.log("before");
-//Async code: will be executed later
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000);
+
+const user = getUser(1);
+console.log(user);
 
 console.log("after");
+// Patterns for dealing with async code.
+// 1. Callbacks
+// 2. Promises
+// 3. Async/await
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        return ({ id: id, gitHubUsername: 'ubaid' });
+    }, 2000);
+}
